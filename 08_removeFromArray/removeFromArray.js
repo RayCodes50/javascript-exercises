432fa63f52e8dd79f5f39f64308d3fd702587b75
RayCodes50 <przemox2@gmail.com>
2026-01-14 14:44:34 +0000

    Create a function that removes arguments from arr

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,21 @@
-const removeFromArray = function() {
-};
-
+// const removeFromArray = function (arr, ...arg) {
+//   for (let i = 0; i < arr.length; i++) {
+//     let exists = false;
+//     for (let j = 0; j < arg.length; j++) {
+//       if (arr[i] === arg[j]) {
+//         exists = true;
+//         break;
+//       }
+//     }
+//     if (exists) {
+//       arr.splice(i, 1);
+//       i--;
+//       //   break;
+//     }
+//   }
+//   return arr;
+// };
+const removeFromArray = (arr, ...args) =>
+  arr.filter((item) => !args.includes(item));
 // Do not edit below this line
 module.exports = removeFromArray;
